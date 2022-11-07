838545f20850630d385fd34a5c1b497f8cb6afd5
Thomas <Jia.Liang.Guo@ocean.ibm.com>
2022-11-07 20:54:30 +0800

    django_react init

--- django_react/settings.py
+++ django_react/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!)bxe75d^n0l^8^(oh)j_&$$ekg02(zq*(h1#iqylxxabr!wo='
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
